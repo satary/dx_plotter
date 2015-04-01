3e0f2a87d761ef91eb09b0c044278c58627a3068
satary <satary@ya.ru>
2015-04-01 16:40:13 +0300

    added license

--- dx_plotter.py
+++ dx_plotter.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
+'''
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-# Form implementation generated from reading ui file 'opener.ui'
-#
-# Created: Fri Dec 05 01:36:58 2014
-#      by: PyQt4 UI code generator 4.11.3
-#
-# WARNING! All changes made in this file will be lost!
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Author = Grigoriy Armeev (MSU)
+'''
 from PyQt4 import QtCore, QtGui
 import sys,os
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
